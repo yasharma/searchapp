9b53e394e10b183063d08b014aeed2e75a137062
yash <yashsharma205@gmail.com>
2016-05-05 22:03:55 +0530

    Fixing JS

---
+++ app/webroot/js/controllers.js
@@ -0,0 +1,54 @@
+angular.module('app.controllers', [])
+	.controller('AppController',function($scope, $http, $location, $rootScope){
+		$rootScope.appURL = 'http://blog.dev';
+	})
+	.controller('PostController',function($scope, $http, $location, $rootScope){
+		var load = function(){
+			$http.get($rootScope.appURL + '/posts.json')
+				.success( function(response){
+					$scope.posts = response.posts;
+			});
+		}
+		
+		load();
+		$scope.deletePost = function(index){
+			var e = $scope.posts[index];
+			$http.delete($rootScope.appURL + '/posts/' + e.Post.id + '.json')
+				.success(function(response){
+					load();
+			});
+		};
+
+		$scope.editPost = function(index){
+			$location.path('/edit/' + $scope.posts[index].Post.id);
+		};
+	})
+	.controller('NewPostController' ,function($scope,$http,$location,$rootScope){		
+		$scope.save = function () {
+			var _data = {};
+			_data.Post = $scope.post;
+			$http.post($rootScope.appURL + '/posts.json', _data)
+				.success(function(response){
+					$location.path('/');
+			});
+		};
+
+		$scope.cancel = function () { $location.path('/'); };
+	})
+	.controller('EditPostController' ,function($scope,$http,$routeParams,$location,$rootScope){
+		$http.get($rootScope.appURL + '/posts/' + $routeParams['id'] + '.json')
+        	.success(function(data) {
+            	$scope.post = data.post.Post;
+        });
+
+        $scope.updatePost = function () {
+			var _data = {};
+			_data.Post = $scope.post;
+			$http.put($rootScope.appURL + '/posts/' + $scope.post.id + '.json', _data)
+				.success(function(response){
+					$location.path('/');
+			});
+		};
+
+		$scope.cancel = function () { $location.path('/'); };
+	});
