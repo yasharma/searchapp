c965b8f9c37f757455ff89c02672c52d1938a196
yash <yashsharma205@gmail.com>
2016-06-14 22:09:00 +0530

    Update Procfile to use node server to execute

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: vendor/bin/heroku-php-nginx -C nginx_app.conf app/webroot/
+web: vendor/bin/heroku-php-nginx -C nginx_app.conf node app.js app/webroot/
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var io = require('socket.io')(8082);
+var port = process.env.PORT || 3000;
+var io = require('socket.io')(port);
 io.on('connection', function (socket) {
 	socket.on('new.post.created', function () {
 		io.emit('new.post.created');
 	});
 });
+console.log('app is listening on port ... ' + port);
