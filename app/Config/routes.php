803b4589ffaaa9bdd0dc8fa3832c4afc29822779
yash <yashsharma205@gmail.com>
2016-06-16 23:06:58 +0530

    Mark category controller as RESTful

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -10,7 +10,7 @@
  * how to customize the loading of plugin routes.
  */
 	CakePlugin::routes();
-	Router::mapResources(array('posts','users'));
+	Router::mapResources(array('posts','users','categories'));
 	Router::parseExtensions('json');
 
 /**
