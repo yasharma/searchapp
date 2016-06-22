479de496c247f48ad88f62da09441a7636180607
yash <yashsharma205@gmail.com>
2016-06-22 20:38:45 +0530

    Add nodejs buildpack

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bin/start-nginx
+web: bin/start-nginx ./node_modules/.bin/forever app.js
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-var io = require('socket.io')(8082);
+var io = require('socket.io')(5001);
 io.on('connection', function (socket) {
 	socket.on('new.post.created', function () {
 		io.emit('new.post.created');
 	});
 });
+Server.listen(‘/tmp/nginx.socket’);
