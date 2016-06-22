c2adfddc7ea9b832e8409a0484c9ab31cac78d2e
yash <yashsharma205@gmail.com>
2016-06-22 21:37:37 +0530

    Fix node server file

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+var fs = require('fs');
 var io = require('socket.io')(process.env.PORT);
 io.on('connection', function (socket) {
 	socket.on('new.post.created', function () {
 		io.emit('new.post.created');
 	});
 });
+fs.openSync('/tmp/app-initialized', 'w');
 Server.listen('/tmp/nginx.socket');
