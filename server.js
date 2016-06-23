d0ae640451e018a222032537e250d86d63a3aa9e
yash <yashsharma205@gmail.com>
2016-06-23 20:45:28 +0530

    Fix: Server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ fs.writeFile("/tmp/app-initialized", "Ready to launch nginx", function(err) {
         console.log("The file was saved!");
     }
 });
+
 var server = http.createServer();
+
 // listen on the nginx socket
 server.listen('/tmp/nginx.socket', function() {
     console.log("Listening ");
